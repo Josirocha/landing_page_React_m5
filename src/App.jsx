dd9d62a135892b1c6582f1be0daa38a0c1704ed8
Josiane Rocha <josi1992@gmail.com>
2022-08-21 23:36:33 -0300

    feat: importing the Header component in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card/Card'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Header/>
+      <Card name="name"
+        description="description"
+        oldPrice="oldPrice"
+        price="price"
+        installments={{
+          count: "12",
+          value: "12,30"
+        }}
+      />
     </div>
   )
 }
