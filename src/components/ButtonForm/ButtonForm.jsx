c75a90a7e295ea365540d4ab01b07a3078441b54
Josiane Rocha <josi1992@gmail.com>
2022-08-23 11:21:12 -0300

    feat: created the ButtonForm component

---
+++ src/components/ButtonForm/ButtonForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ButtonForm.module.css'
+
+const ButtonForm = ({ text }) => {
+    return (
+        <button className={styles.button}>{text}</button>
+    )
+}
+
+export default ButtonForm
