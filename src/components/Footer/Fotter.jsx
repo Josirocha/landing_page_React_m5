9f59beb1fe8f3cb3739c0441b4ea25e2e6ff9afc
Josiane Rocha <josi1992@gmail.com>
2022-08-23 22:34:42 -0300

    feat: created the Footer  component

---
+++ src/components/Footer/Fotter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            
+            <div className={styles.containerP}>
+            <p className={styles.p} >Testando suas habilidades em HTML, CSS e JS.</p>
+            <p >Linx Impulse</p>
+            <p >2019</p>
+            </div>
+            
+            
+        </footer>
+    )
+}
+
+export default Footer
