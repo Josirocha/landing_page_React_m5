512543ea8f97e8d1c46706a0210c208d2d068b2f
Josiane Rocha <josi1992@gmail.com>
2022-08-23 23:22:27 -0300

    style: component styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,11 +8,11 @@ const Card = ({ img, name, description, oldPrice, price, count, value }) => {
             <div className={styles.img}>
                 <img src={img} alt="produto" />
             </div>
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <h4>De:{oldPrice},00</h4>
-            <h5>Por:{price},00</h5>
-            <p>ou {count}x de {value}</p>
+            <h3 className={styles.text}>{name}</h3>
+            <p className={styles.text}>{description}</p>
+            <h4 className={styles.text}>De:{oldPrice},00</h4>
+            <h5 className={styles.text}>Por:{price},00</h5>
+            <p className={styles.text}>ou {count}x de {value}</p>
             <div className={styles.container}>
             <CardButton text='Comprar'/>
             </div>
--- src/components/PreText/PreText.jsx
+++ src/components/PreText/PreText.jsx
@@ -4,7 +4,7 @@ import styles from './PreText.module.css'
 const PreText = () => {
     return (
         <div className={styles.divText}>
-            <h3>Ajude o algorítimo a ser mais certeiro</h3>
+            <h3 className={styles.h3}>Ajude o algorítimo a ser mais certeiro</h3>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies tellus nec mi porta convallis sollicitudin eu urna. Mauris rhoncus erat sed interdum dignissim. Suspendisse semper pretium consectetur. Praesent bibendum arcu risus, sit amet iaculis ex tempus quis. Cras et erat ut tellus vulputate tincidunt. Aenean lacinia euismod augue vel egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vel urna tortor. Vivamus et arcu non felis tristique eleifend.
             </p>
